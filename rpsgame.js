118a6309832916f77f7db986681d0651b78a5f23
Raheel Ahmad <raheelA0@pm.me>
2022-05-25 14:41:12 -0400

    add scorekeeper and display result

--- rpsgame.js
+++ rpsgame.js
@@ -7,35 +7,41 @@ function computerPlay() {
 // playRound function, plays a single round of game
 // takes two parameters: playerSelecton and ComputerSelection
 // returns string declaring winner of round
-function playRound(playerSelecton, computerSelection) {
+function playRound(playerSelection, computerSelection) {
 
-    if(playerSelecton == "rock"){
-        if(computerSelection == "rock"){
+    if(playerSelection == "rock"){
+        if(computerSelection == "rock") {
             return "It's a Tie!"
         }
         else if(computerSelection == "paper") {
+            computerScore += 1;
             return "You Lose! Paper beats Rock"
         }
         else if(computerSelection == "scissors") {
+            playerScore += 1;
             return "You Win! Rock beats Scissors"
         }
     }
-    else if(playerSelecton == "paper"){
-        if(computerSelection == "rock"){
+    else if(playerSelection == "paper") {
+        if(computerSelection == "rock") {
+            playerScore += 1;
             return "You Win! Paper beats Rock"
         }
         else if(computerSelection == "paper") {
             return "It's a Tie!"
         }
         else if(computerSelection == "scissors") {
+            computerScore += 1;
             return "You Lose! Scissors beats Rock"
         }
     }
-    else if(playerSelecton == "scissors"){
-        if(computerSelection == "rock"){
+    else if(playerSelection == "scissors") {
+        if(computerSelection == "rock") {
+            computerScore += 1;
             return "You Lose! Rock beats Scissors"
         }
         else if(computerSelection == "paper") {
+            playerScore += 1;
             return "You Win! Scissors beats Paper"
         }
         else if(computerSelection == "scissors") {
@@ -46,3 +52,29 @@ function playRound(playerSelecton, computerSelection) {
 
 // game function, calls playRound function to play 5 rounds of a game
 // keeps track of score
+function game(){
+    for (let i = 0; i < 5; i++){
+        computerSelection = computerPlay();
+        playerSelection = "rock";
+        playRound(playerSelection, computerSelection);
+    }
+}
+
+function displayResult(){
+    console.log("Player score:", playerScore);
+    console.log("Computer score:", computerScore);
+    if(playerScore == computerScore){
+        console.log("It's a tie!");
+    } 
+    else if(playerScore < computerScore){
+        console.log("Computer Wins!");
+    }
+    else if(playerScore > computerScore){
+        console.log("You Win!");
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+game();
+displayResult();
