a1e26c8855bb9873ede5796f3c8a2530d3403078
Raheel Ahmad <raheelA0@pm.me>
2022-05-25 13:24:54 -0400

    create pseudocode for JS script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rock-paper-scissors
+
+Simple Rock Paper Scissors game played entirely in the console of web browser.
+Practice for JS programming.
---
+++ rpsgame.js
@@ -0,0 +1,8 @@
+// computerPlay function, randomly returns 'Rock','Paper', or 'Scissors'
+
+// playRound function, plays a single round of game
+// takes two parameters: playerSelecton and ComputerSelection
+// returns string declaring winner of round
+
+// game function, calls playRound fucntion to play 5 rounds of a game
+// keeps track of score
