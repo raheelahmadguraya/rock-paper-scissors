4b763e1f60d047cea22eea48d571f7c0a3e5e948
Raheel Ahmad <raheelA0@pm.me>
2022-05-25 13:36:25 -0400

    create function computerPlay to randomly select rps

--- rpsgame.js
+++ rpsgame.js
@@ -1,8 +1,12 @@
 // computerPlay function, randomly returns 'Rock','Paper', or 'Scissors'
+function computerPlay(){
+    const computerSelection = ["rock", "paper", "scissors"];
+    return computerSelection[Math.floor(Math.random()*3)]
+}
 
 // playRound function, plays a single round of game
 // takes two parameters: playerSelecton and ComputerSelection
 // returns string declaring winner of round
 
-// game function, calls playRound fucntion to play 5 rounds of a game
+// game function, calls playRound function to play 5 rounds of a game
 // keeps track of score
